29748ad50c6c682b9f21069a2691f9b27e8fc8e4
cgarwood@jjc.edu <3Tep9jrsxfd$323sdfffg3F>
2022-02-28 14:45:02 -0800

    Create Bird.h, Bird.cpp, main.cpp and cMakeLists.txt files

---
+++ main.cpp
@@ -0,0 +1,14 @@
+// Clinton Garwood
+// Created by ncc306 on 2/28/22.
+// main driver, main.cpp
+
+#include <iostream>
+#include <string>
+#include "Bird.h"
+using namespace std;
+
+int main(){
+    cout << "Tweet tweet" << endl;
+
+    return 0;
+}
